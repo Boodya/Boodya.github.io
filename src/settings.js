bbbaceaaac176316320346e95c94a25bb40e0a51
Ilya <ilya1591@mail.ru>
2018-05-03 12:11:19 +0300

    master

---
+++ src/settings.js
@@ -0,0 +1,29 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {}
+    },
+    launchScene: "db://assets/Scene/helloworld.fire",
+    scenes: [
+        {
+            url: "db://assets/Scene/helloworld.fire",
+            uuid: "2dL3kvpAxJu6GJ7RdqJG5J"
+        }
+    ],
+    packedAssets: {
+        "06d284cc": [
+            "2dL3kvpAxJu6GJ7RdqJG5J",
+            "ad3Ocf4+REmp0Htlp7Pa9x"
+        ]
+    },
+    orientation: "",
+    debug: true
+};
